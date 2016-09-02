4622132f2dd29b82b7d7204fb33ca948c59b2735
Bastian Bechtold <basti@bastibe.de>
2016-09-02 08:31:49 +0200

    adds documentation

--- Token.m
+++ Token.m
@@ -8,6 +8,11 @@ classdef Token < handle
 
     methods
         function obj = Token(type, text, line, col)
+        %TOKEN an atomic piece of source code
+        %   Each token references an atomic piece of source code TEXT at a
+        %   specific LINE and COL. Each TOKEN is tagged as a certain TYPE.
+        %   returns a new OBJ.
+
             obj.type = type;
             obj.text = text;
             obj.line = line;
@@ -15,14 +20,23 @@ classdef Token < handle
         end
 
         function yesNo = hasType(obj, type)
+        %HASTYPE checks it OBJ has matching TYPE
+        %   YESNO is a boolean.
+
             yesNo = any(strcmp(obj.type, type));
         end
 
         function yesNo = hasText(obj, text)
+        %HASTEXT checks it OBJ has matching TEXT
+        %   YESNO is a boolean.
+
             yesNo = any(strcmp(obj.text, text));
         end
 
         function yesNo = isEqual(obj, type, text)
+        %ISEQUAL checks it OBJ has matching TYPE and TEXT
+        %   YESNO is a boolean.
+
             yesNo = obj.hasType(type) && obj.hasText(text);
         end
     end
